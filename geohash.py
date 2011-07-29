79bb5cddca0bdfe45d42940ffb10a345593d97e3
Michael Bemmerl <mail@mx-server.de>
2011-07-29 04:00:52 +0200

    Added ability to use other markets than Mt. Gox.

--- geohash.py
+++ geohash.py
@@ -6,7 +6,7 @@
 # This script is free software, licensed under the GPLv3, of which you should have received a copy with this software.
 # If you didn't, I apologize, but you'll be able to find it at /usr/share/common-licences/GPL-3 or http://www.gnu.org/licenses/gpl-3.0.txt
 
-# This script will calculate a geohash location for you based on the opening price at Mt. Gox for BTC/USD trades.
+# This script will calculate a geohash location for you based on the opening price at a market for BTC trades.
 # Usually, people use the DJIA for this algorithm, but I didn't think that was nerdy enough :)
 # It will also automagically give you a link to a google map to the location.
 # If you'd like any help with it, don't hesitate to open up an issue at github.
@@ -18,29 +18,37 @@ import json
 import urllib
 import argparse
 
-parser = argparse.ArgumentParser(description="Calculate a geohash location based on the opening price for BTC/USD trades at Mt. Gox.")
+parser = argparse.ArgumentParser(description="Calculate a geohash location based on the opening price for BTC trades.")
 parser.add_argument('lat', help="Latitude (integer part)", type=int)
 parser.add_argument('lon', help="Longitude (integer part)", type=int)
+parser.add_argument('-s', '--symbol', help="Symbol of the market (default: mtgoxUSD)", default="mtgoxUSD")
 
 args = parser.parse_args()
 
 latitude = args.lat
 longitude = args.lon
+symbol = args.symbol.lower()
+jsoninfo = ""
 
-btcinfo = urllib.urlopen("http://bitcoincharts.com/t/markets.json")
-jsoninfo = btcinfo.read()
+try:
+        btcinfo = urllib.urlopen("http://bitcoincharts.com/t/markets.json")
+        jsoninfo = btcinfo.read()
+except IOError as (errno, strerror):
+        print "Could not retrieve data from bitcoincharts: " + str(strerror)
+	raise SystemExit
 
 dictinfo = json.loads(jsoninfo)
 
 todayopen = -1
 for sym in dictinfo:
-    if sym["symbol"] == "mtgoxUSD":
+    if sym["symbol"].lower() == symbol:
         todayopen = sym["open"]
+        break
     else:
         continue
 
 if todayopen < 0:
-    raise ValueError("No data from bitcoincharts, is it down?")
+    raise ValueError("Symbol \"{0}\" not found. Try another one.".format(symbol))
 
 thestring = str(datetime.date.today()) + "-" + str(todayopen)
 
